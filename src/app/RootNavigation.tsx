2208ac7fd96471c6e62440259e79026010b73153
이주상 <jusang@ijusang-ui-Macmini.local>
2025-02-23 16:36:13 +0900

    first commit

---
+++ src/app/RootNavigation.tsx
@@ -0,0 +1,14 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+
+const hideTabBarScreens = ['Login'];
+
+const RootNavigation = () => {
+  return (
+    
+  );
+};
+
+export default RootNavigation;
